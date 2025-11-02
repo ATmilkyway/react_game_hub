13374d0f7723a42b2308080c811d74d0ea5f2d49
ATmilkyway <125128752+ATmilkyway@users.noreply.github.com>
2025-11-02 23:05:36 +0300

    optimize card images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,10 +11,13 @@ import {
 import {} from "react-icons";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 const GameCard = ({ game }: { game: Game }) => {
+  console.log(getCroppedImageUrl(game.background_image))
   return (
     <Card.Root maxW="sm" overflow="hidden">
-      <Image src={game.background_image} alt={game.name} />
+      {/* <Image src={game.background_image} alt={game.name} /> */}
+      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <Card.Body gap="2">
         <Card.Title fontSize="2xl">{game.name}</Card.Title>
         <HStack justifyContent="space-between">
