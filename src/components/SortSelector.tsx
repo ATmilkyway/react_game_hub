e0bffa02a9999cd513a07040d78ebdc63a424e8f
ATmilkyway <natnaelasegd0507@example.com>
2025-11-05 16:07:04 +0300

    Sort games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,29 +1,46 @@
 import { Button, Menu, MenuItem, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  selectedSortOrder: string;
+  onSelectSortOrder: (sortorder: string) => void;
+}
+
+const SortSelector = ({ selectedSortOrder, onSelectSortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date adeded" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentOrder = sortOrders.find(
+    (order) => order.value === selectedSortOrder
+  );
+
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="subtle" size="sm">
-          Order by Releveance <BsChevronDown />
+          {currentOrder?.label || "Order by Releveance"} <BsChevronDown />
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content minW="10rem">
             <Menu.RadioItemGroup>
-              <MenuItem value="action">Action</MenuItem>
-              {/* {data.map((item) => (
+              {sortOrders.map((item) => (
                 <Menu.RadioItem
-                  key={item.id}
-                  value={item.name}
-                  onClick={() => onSelectPlatform(item)}
+                  key={item.value}
+                  value={item.value}
+                  onClick={() => onSelectSortOrder(item.value)}
                 >
-                  {item.name}
+                  {item.label}
                   <Menu.ItemIndicator />
                 </Menu.RadioItem>
-              ))} */}
+              ))}
             </Menu.RadioItemGroup>
           </Menu.Content>
         </Menu.Positioner>
