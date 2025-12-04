1f00998a71c9e2c73a61e7c2e505d2e0137b2b59
ATmilkyway <natnaelasegd0507@example.com>
2025-12-04 11:26:52 +0300

    Feat: display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import type { Game } from "@/hooks/useGames";
 import { Button, Card, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -12,8 +13,12 @@ const GameCard = ({ game }: Props) => {
         alt="Green double couch with wooden legs"
       />
       <Card.Body gap="2">
-        <Card.Title fontSize='2xl'>{game.name}</Card.Title>
-        <Card.Description></Card.Description>
+        <Card.Title fontSize="2xl">{game.name}</Card.Title>
+        <Card.Description>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+        </Card.Description>
       </Card.Body>
       <Card.Footer gap="2"></Card.Footer>
     </Card.Root>
