5f3ead49ff2a9ea29b0bbd9ccedc7562e09ae79b
ATmilkyway <natnaelasegd0507@example.com>
2025-12-04 16:56:32 +0300

    Feat: show a spinner for genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,14 +1,16 @@
 import useGenres from "@/hooks/useGenres";
-import { HStack, Image, List } from "@chakra-ui/react";
+import { HStack, Image, List, Spinner } from "@chakra-ui/react";
 
 const GenresList = () => {
   const { data, error, isLoading } = useGenres();
+  if (isLoading) return <Spinner size="md" />;
+  if (error) return null;
   return (
     <List.Root>
       {data.map((genre) => (
-        <HStack key={genre.id} padding='5px'>
-          <Image src={genre.image_background} boxSize="32px" borderRadius={8}/>
-          <List.Item fontSize='lg'>{genre.name}</List.Item>
+        <HStack key={genre.id} padding="5px">
+          <Image src={genre.image_background} boxSize="32px" borderRadius={8} />
+          <List.Item fontSize="lg">{genre.name}</List.Item>
         </HStack>
       ))}
     </List.Root>
