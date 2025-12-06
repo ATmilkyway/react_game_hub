cf38106c241f65a0bbfd08514791e1e590750b68
ATmilkyway <natnaelasegd0507@example.com>
2025-12-06 13:10:46 +0300

    UI: update theme

--- src/theme.ts
+++ src/theme.ts
@@ -8,7 +8,20 @@ import {
 const config: SystemConfig = defineConfig({
   theme: {
     tokens: {
-      colors: {},
+      colors: {
+        gray: {
+          50: { value: "#f2f2f2" },
+          100: { value: "#d9d9d9" },
+          200: { value: "#bfbfbf" },
+          300: { value: "#a6a6a6" },
+          400: { value: "#8c8c8c" },
+          500: { value: "#737373" },
+          600: { value: "#595959" },
+          700: { value: "#404040" },
+          800: { value: "#262626" },
+          900: { value: "#0d0d0d" },
+        },
+      },
     },
   },
 });
