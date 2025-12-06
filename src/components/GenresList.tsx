ee3f5c4d89bcfa91c3e146c5ac9704327e5b6cfe
ATmilkyway <natnaelasegd0507@example.com>
2025-12-06 11:40:21 +0300

    Refactor: clean up genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,5 +1,12 @@
 import useGenres, { type Genre } from "@/hooks/useGenres";
-import { Button, HStack, Image, List, Spinner } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  Spinner,
+} from "@chakra-ui/react";
 
 interface Props {
   selectedGenre: Genre | null;
@@ -11,27 +18,37 @@ const GenresList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner size="md" />;
   if (error) return null;
   return (
-    <List.Root listStyle="none">
-      {data.map((genre) => (
-        <List.Item key={genre.id}>
-          <HStack padding="5px">
-            <Image
-              src={genre.image_background}
-              boxSize="32px"
-              borderRadius={8}
-            />
-            <Button
-              fontSize="lg"
-              variant="plain"
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List.Root listStyle="none">
+        {data.map((genre) => (
+          <List.Item key={genre.id}>
+            <HStack padding="5px">
+              <Image
+                src={genre.image_background}
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+              />
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontSize="lg"
+                variant="plain"
+                onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                maxW="150px"
+                paddingX="0"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
