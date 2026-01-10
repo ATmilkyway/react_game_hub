46cd522fd2158bee0824e19df86904000b1c4ecf
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-10 14:29:48 +0300

    feat: build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,18 @@
+import useTrailers from "@/hooks/useTrailers";
+import { Spinner } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useTrailers(gameId);
+  const first = data?.results[0];
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls></video>
+  ) : null;
+};
+
+export default GameTrailer;
