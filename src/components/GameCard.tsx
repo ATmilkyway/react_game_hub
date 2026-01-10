99eca79025bef501a65e92fc8629d9f69e2eb9f3
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-10 12:24:31 +0300

    feat: fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,19 +4,22 @@ import {} from "react-icons";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "@/services/image-url";
+import { Link } from "react-router-dom";
 const GameCard = ({ game }: { game: Game }) => {
   return (
     <Card.Root maxW="sm" overflow="hidden">
       {/* <Image src={game.background_image} alt={game.name} /> */}
       <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <Card.Body gap="2" width="100%">
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map(({ platform }) => platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Card.Title fontSize="2xl">{game.name}</Card.Title>
+        <Link to={`/games/${game.slug}`}>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map(({ platform }) => platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Card.Title fontSize="2xl">{game.name}</Card.Title>
+        </Link>
       </Card.Body>
     </Card.Root>
   );
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,14 @@ interface Props {
 }
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box maxW="sm" overflow="hidden">
+    <Box
+      maxW="sm"
+      overflow="hidden"
+      transition="transform .15s ease-in"
+      _hover={{
+        transform: "scale(1.02)",
+      }}
+    >
       {children}
     </Box>
   );
