698f44858faeb87bdacaafafb301736f17264068
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-10 16:00:11 +0300

    feat: build game screenshots

---
+++ src/hooks/useScreenShots.ts
@@ -0,0 +1,13 @@
+import type { ScreenShots } from "@/entities/ScreenShots";
+import APIClient from "@/services/api-client";
+import { useQuery } from "@tanstack/react-query";
+
+const useScreenShots = (gameId: number) => {
+  const apiClient = new APIClient<ScreenShots>(`/games/${gameId}/screenshots`);
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useScreenShots;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
 import ExpandableText from "@/components/ExpandableText";
 import GameAttributes from "@/components/GameAttributes";
+import GameScreenShots from "@/components/GameScreenShots";
 import GameTrailer from "@/components/GameTrailer";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
@@ -19,7 +20,8 @@ const GameDetailPage = () => {
       </Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
-      <GameTrailer gameId={game.id}/>
+      <GameTrailer gameId={game.id} />
+      <GameScreenShots gameId={game.id} />
     </>
   );
 };
